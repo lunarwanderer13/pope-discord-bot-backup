84b4675cac27be5941152cbdaaacb397b6aea476
LunarWanderer <lunarwanderer13@gmail.com>
2025-11-03 16:39:30 +0100

    Removed unnecessary comments and old file

--- src/events/pope.js
+++ src/events/pope.js
@@ -1,10 +1,3 @@
-// export const name = "pope";
-// export const once = false;
-// export const on = true;
-// export function execute(client) {
-//     console.log(`Logged in as ${client.user.tag}!`);
-// }
-
 export default {
     name: "messageCreate",
     once: false,
--- src/events/ready.js
+++
@@ -1,5 +0,0 @@
-export const name = "clientReady";
-export const once = true;
-export function execute(client) {
-    console.log(`Logged in as ${client.user.tag}!`);
-}
