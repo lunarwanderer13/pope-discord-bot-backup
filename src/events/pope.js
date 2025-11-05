b1a0bde56d84e7f0438f6e0d0f0667f1f4e96552
LunarWanderer <lunarwanderer13@gmail.com>
2025-11-05 20:08:47 +0100

    Added pictures and emoji to 2137

--- src/events/pope.js
+++ src/events/pope.js
@@ -1,8 +1,17 @@
+import { AttachmentBuilder } from "discord.js"
 import fs from "fs"
+import "dotenv/config"
 
 export const name = "messageCreate"
 export const once = false
 export function execute(message) {
+    const kremufki = [
+        "src\\images\\kremufka1.png",
+        "src\\images\\kremufka2.png",
+        "src\\images\\kremufka3.png",
+        "src\\images\\kremufka4.png"
+    ]
+
     let now = new Date()
     const hours = now.getHours()
     const minutes = now.getMinutes()
@@ -35,9 +44,14 @@ export function execute(message) {
 
             let reply_message = `${message.author} to twoja ${entry.popes} papieżowa, `
             if (entry.popes_in_a_row > 1) reply_message += `już ${entry.popes_in_a_row} z rzędu, `
-            reply_message += "trzymaj kremówkę! 🍮"
+            reply_message += `trzymaj kremówkę! <:kremuuuuufkuuuj_z_tyyyyyym_:1435705908167708743>`
 
-            message.reply(reply_message)
+            const attachment = new AttachmentBuilder(kremufki[Math.floor(Math.random() * kremufki.length)], { name: "kremufka.png" })
+
+            message.reply({
+                content: reply_message,
+                files: [attachment]
+            })
         } else {
             message.reply(`${message.author} nieco za szybko piszesz tą godzine, może poczekaj do jutra co?`)
         }
